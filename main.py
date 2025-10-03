d0aec85f49aa7ef1881714736495359ff0d77a75
Jaime Garriga Gonzalez <jaime.garriga.contacto@gmail.com>
2025-10-03 12:14:13 +0200

    Main mejorado

--- main.py
+++ main.py
@@ -8,19 +8,46 @@ while opcion != 0:
     opcion = int(input("Selecciona opción: "))
 
     if opcion == 1:
-        nombreCarrera = input("Inserta nombre de la carrera: ")
-        sql = "INSERT INTO carreras (nombre) VALUES (%s)"
-        valores = [nombreCarrera]
+        nombre = input("Inserta nombre de la carrera: ")
+        grado = input("Inserta el grado de la carrera: ")
+        rama = input("Inserta la rama de la carrera: ")
+        carrera = Carrera(nombre, grado, rama)
+        sql = "INSERT INTO carreras (nombre, grado, rama) VALUES (%s, %s, %s)"
+        valores = (carrera.getNombre(), carrera.getGrado(), carrera.getRama())
         mycursor.execute(sql, valores)
-
         mydb.commit()
-        print(f"Se ha(n) insertado {mycursor.rowcount} fila(s).")
 
-    if opcion == 3:
-        nombreCarrera = input("Inserta nombre de la carrera: ")
-        sql = f"SELECT * FROM carreras WHERE nombre LIKE '{nombreCarrera}'"
+        print(f"Se ha insertado {mycursor.rowcount} fila.")
+
+    elif opcion == 2:
+        idCarrera = int(input("Inserta el ID de la carrera a actualizar: "))
+        nuevoNombre = input("Nuevo nombre: ")
+        nuevoGrado = input("Nuevo grado: ")
+        nuevaRama = input("Nueva rama: ")
+        carrera = Carrera(nuevoNombre, nuevoGrado, nuevaRama)
+        sql = "UPDATE carreras SET nombre = %s, grado = %s, rama = %s WHERE idcarreras = %s"
+        valores = (carrera.getNombre(), carrera.getGrado(), carrera.getRama(), idCarrera)
+        mycursor.execute(sql, valores)
+        mydb.commit()
+
+        print(f"Se ha actualizado {mycursor.rowcount} fila.")
+
+    elif opcion == 3:
+        sql = "SELECT * FROM carreras"
         mycursor.execute(sql)
         consulta = mycursor.fetchall()
-
+        print("Lista de carreras:")
         for fila in consulta:
-            print(fila)
+            print(f"ID: {fila[0]}, Nombre: {fila[1]}, Grado: {fila[2]}, Rama: {fila[3]}")
+
+    elif opcion == 4:
+        idCarrera = int(input("Inserta el ID de la carrera a borrar: "))
+        sql = "DELETE FROM carreras WHERE idcarreras = %s"
+        valores = (idCarrera,)
+        mycursor.execute(sql, valores)
+        mydb.commit()
+
+        print(f"Se ha borrado {mycursor.rowcount} fila.")
+
+    elif opcion == 0:
+        print("Saliendo del programa...")
